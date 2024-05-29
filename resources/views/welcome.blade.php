de122e81d7fec4ade37cb0a178897e0b7ca4dce7
abdirazack <abdirizakomar254@gmail.com>
2024-05-29 13:12:29 +0300

    Refactor welcome.blade.php to improve code readability and maintainability

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,25 +5,31 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+        {{-- <link rel="icon" href="{{ asset('logo.svg') }}"> --}}
+ <link rel="icon" href="{{ asset('logo.svg') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <wireui:scripts />
+
     <!-- Scripts -->
-     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="antialiased">
-    <div>
+<body class="antialiased h-screen flex flex-col">
+    <div class="absolute top-0 right-0 p-6">
         @if (Route::has('login'))
             <livewire:welcome.navigation />
         @endif
-        <div class="flex flex-col items-center justify-center p-6 mx-auto space-y-4 text-center max-w-7xl lg:p-8">
+    </div>
+
+    <div class="flex-grow flex items-center justify-center">
+        <div class="flex flex-col items-center justify-center space-y-4">
             <x-application-logo class="w-24 h-24 fill-current text-primary" />
             <x-button primary xl href="{{ route('register') }}">Get Started</x-button>
         </div>
+    </div>
 </body>
 
 </html>
