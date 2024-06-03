7d0b5d1a2c7a2b264283fd1dd092c23dd33a8c8a
abdirazack <abdirizakomar254@gmail.com>
2024-06-03 16:56:53 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 20000, // Increase the chunk size warning limit
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
     plugins: [
         laravel({
             input: [
